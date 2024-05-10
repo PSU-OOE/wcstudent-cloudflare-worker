859c08b5d5c88853bca142ba05ca09c2a1edf7f9
Luke Leber <lal65@psu.edu>
2024-05-10 15:47:18 -0400

    [almost] initial commit; should be a working action

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,33 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
 export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
-	},
-};
+  async fetch(request: Request): Promise<Response> {
+
+    // Fetch the original response from the origin server
+    let response: Response = await fetch(request);
+
+    // Only react to requests that have an affiliation query parameter of 'student'.
+    if (new URL(request.url).searchParams.get('affiliation') === 'student') {
+      let faculty_or_staff = false;
+      const cookieString = request.headers.get('Cookie') ?? '';
+      const cookies = cookieString.split(';');
+      for (const cookie of cookies) {
+        if (cookie === 'affiliation=staff' || cookie === 'affiliation=faculty') {
+          faculty_or_staff = true;
+          break;
+        }
+      }
+
+      // Only set the student affiliation for non-faculty, non-staff members.
+      if (!faculty_or_staff) {
+        response = new Response(response.body, {
+          status: response.status,
+          statusText: response.statusText,
+          headers: response.headers
+        });
+        response.headers.append('Set-Cookie', 'affiliation=student; Max-Age=31536000; Path=/; Domain=.worldcampus.psu.edu; HttpOnly');
+        response.headers.set('X-Affiliation', 'student');
+      }
+    }
+
+    return response;
+  },
+} satisfies ExportedHandler;
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,25 +1,29 @@
-// test/index.spec.ts
-import { env, createExecutionContext, waitOnExecutionContext, SELF } from 'cloudflare:test';
-import { describe, it, expect } from 'vitest';
-import worker from '../src/index';
+import {SELF} from 'cloudflare:test';
+import {describe, expect, it} from 'vitest';
 
-// For now, you'll need to do something like this to get a correctly-typed
-// `Request` to pass to `worker.fetch()`.
-const IncomingRequest = Request<unknown, IncomingRequestCfProperties>;
+describe('World Campus Student Cloudflare Worker', () => {
 
-describe('Hello World worker', () => {
-	it('responds with Hello World! (unit style)', async () => {
-		const request = new IncomingRequest('http://example.com');
-		// Create an empty context to pass to `worker.fetch()`.
-		const ctx = createExecutionContext();
-		const response = await worker.fetch(request, env, ctx);
-		// Wait for all `Promise`s passed to `ctx.waitUntil()` to settle before running test assertions
-		await waitOnExecutionContext(ctx);
-		expect(await response.text()).toMatchInlineSnapshot(`"Hello World!"`);
-	});
+  it('responds with no affiliation if not specified in the query params', async () => {
+    const response = await SELF.fetch('https://example.com');
+    expect(await response.headers.get('X-Affiliation')).equals(null);
+  });
 
-	it('responds with Hello World! (integration style)', async () => {
-		const response = await SELF.fetch('https://example.com');
-		expect(await response.text()).toMatchInlineSnapshot(`"Hello World!"`);
-	});
+  it('responds with no affiliation if previously identified as a staff member', async () => {
+    const request = new Request('https://example.com?affiliation=student');
+    request.headers.set('Cookie', 'affiliation=staff');
+    const response = await SELF.fetch(request);
+    expect(await response.headers.get('X-Affiliation')).equals(null);
+  });
+
+  it('responds with no affiliation if previously identified as a faculty member', async () => {
+    const request = new Request('https://example.com?affiliation=student');
+    request.headers.set('Cookie', 'affiliation=faculty');
+    const response = await SELF.fetch(request);
+    expect(await response.headers.get('X-Affiliation')).equals(null);
+  });
+
+  it('responds with affiliation if a student affiliation link is visited', async () => {
+    const response = await SELF.fetch('https://example.com?affiliation=student');
+    expect(await response.headers.get('X-Affiliation')).equals('student');
+  });
 });
