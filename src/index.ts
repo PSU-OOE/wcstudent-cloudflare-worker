9611b9c637aaf1fab15b2557e14cdfa3f1c146e9
Luke Leber <lal65@psu.edu>
2025-01-07 11:32:55 -0500

    Use bit packing for affiliations

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,48 @@
 export default {
+
   async fetch(request: Request): Promise<Response> {
 
-    // Fetch the original response from the origin server
-    let response: Response = await fetch(request);
+    // Multiple affiliations are packed into a single cookie because there are
+    // a finite number of cookies that can be used to segment and personalize
+    // the user journey on the Acquia platform.
+    //
+    // See https://docs.acquia.com/acquia-cloud-platform/performance/varnish/cookies
+	  const STUDENT_AFFILIATION_MASK = 0x1;
+    // const STAFF_AFFILIATION_MASK = 0x2;
+    // const MILITARY_AFFILIATION_MASK = 0x4;
 
-    let faculty_or_staff = false;
+    // Assume no prior affiliations initially.
+    let affiliations = 0x0;
+
+    // If there is already an affiliation, use that instead.
     const cookieString = request.headers.get('Cookie') ?? '';
     const cookies = cookieString.split(';');
     for (const cookie of cookies) {
-      if (cookie === 'acquia_a=staff' || cookie === 'acquia_a=faculty') {
-        faculty_or_staff = true;
+      const [name, value] = cookie.split('=');
+      if (name === 'acquia_a') {
+        affiliations = Number(value);
         break;
       }
     }
 
-    // Only set the student affiliation for non-faculty, non-staff members.
-    if (!faculty_or_staff) {
-      response = new Response(response.body, {
-        status: response.status,
-        statusText: response.statusText,
-        headers: response.headers
-      });
-      response.headers.append('Set-Cookie', 'acquia_a=student; Max-Age=31536000; Path=/; Domain=.' + new URL(request.url).hostname + '; HttpOnly');
-      response.headers.set('X-Affiliation', 'student');
-    }
+    // Add the student affiliation.
+    affiliations |= STUDENT_AFFILIATION_MASK;
 
-    return response;
+    // Extract several parts of the original request...
+	  const { origin, search } = new URL(request.url);
+
+    // Redirect to the proper location while setting a long-lived affiliation
+    // cookie. The cookie is set on the .worldcampus.psu.edu domain for ~13
+    // months -- the practical maximum age that cookies can live for. If no
+    // destination query parameter was provided, redirect the user to the
+    // origin homepage.
+    return new Response(null, {
+      status: 302,
+      headers: {
+        'Location': origin + ((new URLSearchParams(search)).get('destination') ?? '/'),
+        'Set-Cookie': 'acquia_a=' + String(affiliations) + '; Max-Age=34560000; Path=/; Domain=.worldcampus.psu.edu; Secure; HttpOnly',
+        'X-Affiliations': String(affiliations),
+      }
+    });
   },
 } satisfies ExportedHandler;
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -3,22 +3,43 @@ import {describe, expect, it} from 'vitest';
 
 describe('World Campus Student Cloudflare Worker', () => {
 
-  it('responds with no affiliation if previously identified as a staff member', async () => {
-    const request = new Request('https://example.com?affiliation=student');
-    request.headers.set('Cookie', 'acquia_a=staff');
-    const response = await SELF.fetch(request);
-    expect(await response.headers.get('X-Affiliation')).equals(null);
+  it('adds the student affiliation if the user is unaffiliated', async () => {
+    const request = new Request('https://example.com');
+    const response = await SELF.fetch(request, { redirect: 'manual' });
+    expect(await response.headers.get('X-Affiliations')).equals('1');
   });
 
-  it('responds with no affiliation if previously identified as a faculty member', async () => {
-    const request = new Request('https://example.com?affiliation=student');
-    request.headers.set('Cookie', 'acquia_a=faculty');
-    const response = await SELF.fetch(request);
-    expect(await response.headers.get('X-Affiliation')).equals(null);
+  it('adds the student affiliation to existing staff affiliation', async () => {
+    const request = new Request('https://example.com');
+    request.headers.set('Cookie', 'acquia_a=2');
+    const response = await SELF.fetch(request, { redirect: 'manual' });
+    expect(await response.headers.get('X-Affiliations')).equals('3');
   });
 
-  it('responds with affiliation if a student affiliation link is visited', async () => {
-    const response = await SELF.fetch('https://example.com?affiliation=student');
-    expect(await response.headers.get('X-Affiliation')).equals('student');
+  it('adds the student affiliation to existing military affiliation', async () => {
+    const request = new Request('https://example.com');
+    request.headers.set('Cookie', 'acquia_a=4');
+    const response = await SELF.fetch(request, { redirect: 'manual' });
+    expect(await response.headers.get('X-Affiliations')).equals('5');
   });
+
+  it('adds the student affiliation to existing staff + military affiliations', async () => {
+    const request = new Request('https://example.com');
+    request.headers.set('Cookie', 'acquia_a=6');
+    const response = await SELF.fetch(request, { redirect: 'manual' });
+    expect(await response.headers.get('X-Affiliations')).equals('7');
+  });
+
+  it('redirects to the homepage if no destination is provided', async () => {
+    const request = new Request('https://example.com');
+    const response = await SELF.fetch(request, { redirect: 'manual' });
+    expect(await response.headers.get('Location')).equals('https://example.com/');
+  });
+
+  it('redirects to a custom destination if provided', async () => {
+    const request = new Request('https://example.com?destination=/custom-url');
+    const response = await SELF.fetch(request, { redirect: 'manual' });
+    expect(await response.headers.get('Location')).equals('https://example.com/custom-url');
+  });
+
 });
